c4261f49de9351ece53c48e95c85b1ed640efa63
arias-mindfullife <152916321+arias-mindfullife@users.noreply.github.com>
2025-04-11 11:35:01 -0500

    se oculta la seccion collection

--- tienda-noir-frontend/src/pages/Home.js
+++ tienda-noir-frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import Hero from '../components/Hero';
-import Collection from '../components/Collection';
+//import Collection from '../components/Collection';
 import About from '../components/About';
 import CTA from '../components/CTA';
 import Footer from '../components/Footer';
